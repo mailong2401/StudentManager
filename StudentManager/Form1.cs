8c6ff7d1b7f390d8875a2e464a9f2e3f3c2472c3
mailong2401 <gtmailong@gmail.com>
2025-04-29 21:55:05 +0700

    .

--- StudentManager/Form1.cs
+++ StudentManager/Form1.cs
@@ -6,5 +6,10 @@ namespace StudentManager
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
