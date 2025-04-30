9fc6e2edf77693d14a34ca4ba5b54c8494d1cccb
mailong2401 <gtmailong@gmail.com>
2025-04-30 18:13:43 +0700

    .

---
+++ StudentManager/fLogin.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace StudentManager
+{
+    partial class fLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fLogin));
+            panel1 = new Panel();
+            btnExit = new ReaLTaiizor.Controls.CyberButton();
+            btnLogin = new ReaLTaiizor.Controls.CyberButton();
+            cyberGroupBox2 = new ReaLTaiizor.Controls.CyberGroupBox();
+            pictureBox3 = new PictureBox();
+            inputPass = new TextBox();
+            cyberGroupBox1 = new ReaLTaiizor.Controls.CyberGroupBox();
+            pictureBox2 = new PictureBox();
+            inputAcc = new TextBox();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            cyberGroupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            cyberGroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackgroundImage = Properties.Resources.backgroundLogin2;
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(btnExit);
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(cyberGroupBox2);
+            panel1.Controls.Add(cyberGroupBox1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Right;
+            panel1.ForeColor = SystemColors.ControlText;
+            panel1.Location = new Point(517, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(502, 577);
+            panel1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            btnExit.Alpha = 20;
+            btnExit.BackColor = Color.Transparent;
+            btnExit.Background = true;
+            btnExit.Background_WidthPen = 4F;
+            btnExit.BackgroundPen = true;
+            btnExit.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnExit.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnExit.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnExit.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnExit.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnExit.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnExit.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnExit.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnExit.Effect_1 = true;
+            btnExit.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnExit.Effect_1_Transparency = 25;
+            btnExit.Effect_2 = true;
+            btnExit.Effect_2_ColorBackground = Color.White;
+            btnExit.Effect_2_Transparency = 20;
+            btnExit.Font = new Font("Arial", 11F);
+            btnExit.ForeColor = Color.FromArgb(245, 245, 245);
+            btnExit.Lighting = false;
+            btnExit.LinearGradient_Background = false;
+            btnExit.LinearGradientPen = false;
+            btnExit.Location = new Point(284, 396);
+            btnExit.Name = "btnExit";
+            btnExit.PenWidth = 15;
+            btnExit.Rounding = true;
+            btnExit.RoundingInt = 70;
+            btnExit.Size = new Size(155, 61);
+            btnExit.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnExit.TabIndex = 5;
+            btnExit.Tag = "Cyber";
+            btnExit.TextButton = "Thoát";
+            btnExit.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnExit.Timer_Effect_1 = 5;
+            btnExit.Timer_RGB = 300;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Alpha = 20;
+            btnLogin.BackColor = Color.Transparent;
+            btnLogin.Background = true;
+            btnLogin.Background_WidthPen = 4F;
+            btnLogin.BackgroundPen = true;
+            btnLogin.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnLogin.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnLogin.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnLogin.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnLogin.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnLogin.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnLogin.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnLogin.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnLogin.Effect_1 = true;
+            btnLogin.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnLogin.Effect_1_Transparency = 25;
+            btnLogin.Effect_2 = true;
+            btnLogin.Effect_2_ColorBackground = Color.White;
+            btnLogin.Effect_2_Transparency = 20;
+            btnLogin.Font = new Font("Arial", 11F);
+            btnLogin.ForeColor = Color.FromArgb(245, 245, 245);
+            btnLogin.Lighting = false;
+            btnLogin.LinearGradient_Background = false;
+            btnLogin.LinearGradientPen = false;
+            btnLogin.Location = new Point(86, 396);
+            btnLogin.Name = "btnLogin";
+            btnLogin.PenWidth = 15;
+            btnLogin.Rounding = true;
+            btnLogin.RoundingInt = 70;
+            btnLogin.Size = new Size(155, 61);
+            btnLogin.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnLogin.TabIndex = 4;
+            btnLogin.Tag = "Cyber";
+            btnLogin.TextButton = "Đăng nhập";
+            btnLogin.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnLogin.Timer_Effect_1 = 5;
+            btnLogin.Timer_RGB = 300;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // cyberGroupBox2
+            // 
+            cyberGroupBox2.Alpha = 20;
+            cyberGroupBox2.BackColor = Color.Transparent;
+            cyberGroupBox2.Background = true;
+            cyberGroupBox2.Background_WidthPen = 3F;
+            cyberGroupBox2.BackgroundPen = true;
+            cyberGroupBox2.ColorBackground = Color.FromArgb(37, 52, 68);
+            cyberGroupBox2.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            cyberGroupBox2.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            cyberGroupBox2.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            cyberGroupBox2.ColorLighting = Color.FromArgb(29, 200, 238);
+            cyberGroupBox2.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            cyberGroupBox2.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            cyberGroupBox2.Controls.Add(pictureBox3);
+            cyberGroupBox2.Controls.Add(inputPass);
+            cyberGroupBox2.CyberGroupBoxStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            cyberGroupBox2.ForeColor = Color.FromArgb(245, 245, 245);
+            cyberGroupBox2.Lighting = false;
+            cyberGroupBox2.LinearGradient_Background = false;
+            cyberGroupBox2.LinearGradientPen = false;
+            cyberGroupBox2.Location = new Point(86, 300);
+            cyberGroupBox2.Name = "cyberGroupBox2";
+            cyberGroupBox2.PenWidth = 15;
+            cyberGroupBox2.RGB = false;
+            cyberGroupBox2.Rounding = true;
+            cyberGroupBox2.RoundingInt = 60;
+            cyberGroupBox2.Size = new Size(353, 57);
+            cyberGroupBox2.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            cyberGroupBox2.TabIndex = 3;
+            cyberGroupBox2.Tag = "Cyber";
+            cyberGroupBox2.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            cyberGroupBox2.Timer_RGB = 300;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(16, 15);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(27, 27);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 1;
+            pictureBox3.TabStop = false;
+            // 
+            // inputPass
+            // 
+            inputPass.BackColor = Color.FromArgb(37, 52, 68);
+            inputPass.BorderStyle = BorderStyle.None;
+            inputPass.ForeColor = Color.White;
+            inputPass.Location = new Point(62, 15);
+            inputPass.Name = "inputPass";
+            inputPass.Size = new Size(266, 27);
+            inputPass.TabIndex = 2;
+            inputPass.UseSystemPasswordChar = true;
+            // 
+            // cyberGroupBox1
+            // 
+            cyberGroupBox1.Alpha = 20;
+            cyberGroupBox1.BackColor = Color.Transparent;
+            cyberGroupBox1.Background = true;
+            cyberGroupBox1.Background_WidthPen = 3F;
+            cyberGroupBox1.BackgroundPen = true;
+            cyberGroupBox1.ColorBackground = Color.FromArgb(37, 52, 68);
+            cyberGroupBox1.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            cyberGroupBox1.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            cyberGroupBox1.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            cyberGroupBox1.ColorLighting = Color.FromArgb(29, 200, 238);
+            cyberGroupBox1.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            cyberGroupBox1.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            cyberGroupBox1.Controls.Add(pictureBox2);
+            cyberGroupBox1.Controls.Add(inputAcc);
+            cyberGroupBox1.CyberGroupBoxStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            cyberGroupBox1.ForeColor = Color.FromArgb(245, 245, 245);
+            cyberGroupBox1.Lighting = false;
+            cyberGroupBox1.LinearGradient_Background = false;
+            cyberGroupBox1.LinearGradientPen = false;
+            cyberGroupBox1.Location = new Point(86, 204);
+            cyberGroupBox1.Name = "cyberGroupBox1";
+            cyberGroupBox1.PenWidth = 15;
+            cyberGroupBox1.RGB = false;
+            cyberGroupBox1.Rounding = true;
+            cyberGroupBox1.RoundingInt = 60;
+            cyberGroupBox1.Size = new Size(353, 57);
+            cyberGroupBox1.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            cyberGroupBox1.TabIndex = 2;
+            cyberGroupBox1.Tag = "Cyber";
+            cyberGroupBox1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            cyberGroupBox1.Timer_RGB = 300;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(16, 15);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(27, 27);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // inputAcc
+            // 
+            inputAcc.BackColor = Color.FromArgb(37, 52, 68);
+            inputAcc.BorderStyle = BorderStyle.None;
+            inputAcc.ForeColor = Color.White;
+            inputAcc.Location = new Point(62, 15);
+            inputAcc.Name = "inputAcc";
+            inputAcc.Size = new Size(266, 27);
+            inputAcc.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(115, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(276, 159);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackgroundImage = Properties.Resources.backgroundLogin1;
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(524, 577);
+            panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1019, 577);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            cyberGroupBox2.ResumeLayout(false);
+            cyberGroupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            cyberGroupBox1.ResumeLayout(false);
+            cyberGroupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private TextBox inputAcc;
+        private ReaLTaiizor.Controls.CyberGroupBox cyberGroupBox1;
+        private ReaLTaiizor.Controls.CyberGroupBox cyberGroupBox2;
+        private PictureBox pictureBox3;
+        private TextBox inputPass;
+        private ReaLTaiizor.Controls.CyberButton btnExit;
+        private ReaLTaiizor.Controls.CyberButton btnLogin;
+        private PictureBox pictureBox2;
+    }
+}
